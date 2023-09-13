9e71dfcb0c303d25f511c6bf15bf616f05138436
BlackDani557 <freitascosta207@gmail.com>
2023-09-13 17:03:07 -0300

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+var lista_de_produto = []
+
+while (true) {
+    var opcao = prompt(`bem vindo ao cadastro dos produtos :)
+
+    1.cadastrar novo produto 
+    2.editar produto
+    3. remover produto
+    4. visualizar todos os produtos
+`)
+    if (opcao == 1) {
+        //codigo para adicionar um produto
+    } else if (opcao == 2) {
+        //codigo para editar um produto 
+    } else if (opcao == 3) {
+        //codigo para remover
+    } else if (opcao == 4) {
+        //codigo para visualizar
+    } else {
+        alert("nenhuma das opçoes foram selecionadas!")
+    }
+}
