89935f6bf03597b2264962d3f454cecd6b2f71eb
BlackDani557 <143741099+BlackDani557@users.noreply.github.com>
2023-09-27 16:06:22 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ while (true) {
         //codigo para visualizar
     } else {
         alert("nenhuma das opçoes foram selecionadas!")
+        exit()
     }
 }
 
@@ -33,7 +34,7 @@ function adicionar() {
         marca: ``,
         preco: ``,
     }
-    objeto.name = prompt("name")
+    objeto.nome = prompt("nome")
     objeto.marca = prompt("marca")
     objeto.preco = prompt("preço")
     lista_de_produto.push(objeto)
@@ -42,25 +43,43 @@ function adicionar() {
 function editar() {
     var indice = prompt("Qual item voce deseja editar ?")
 
-    lista_de_produto[indice].name = prompt("Edite o nome")
+    lista_de_produto[indice].nome = prompt("editar o nome");
 
-    lista_de_produto[indice].preco = prompt("edite o preço")
+    lista_de_produto[indice].preco = prompt("editar o preco");
 
-    lista_de_produto[indice].marca = prompt("edite a marca")
+    lista_de_produto[indice].marca = prompt("editar a marca");
 
+  var produto = lista_de_produto[indice]
 
+    alert(produto)  
+}
+function remover() {
+    var produto_a_remover = prompt("Selecione o produto a ser removido:\n\n" + listarProdutosParaRemocao());
+
+    for (let i = 0; i < lista_de_produto.length; i++) {
+        if (lista_de_produto[i].nome === produto_a_remover) {
+            lista_de_produto.splice(i, 1);
+            alert("Produto removido com sucesso!");
+            return;
+        }
+    }
+
+    alert("Produto não encontrado.");
+}
+
+function listarProdutosParaRemocao() {
+    var lista = "Produtos disponíveis para remoção:\n\n";
+    for (let i = 0; i < lista_de_produto.length; i++) {
+        lista += lista_de_produto[i].nome + "\n";
+    }
+    return lista;
 }
 
 function ver() {
-    var produto =""
+    var produto = ""
     for (let i = 0; i < 1; i++) {
-        produto += `name: ${lista_de_produto[i].name} produto: ${lista_de_produto[i].marca} preço ${lista_de_produto[i].preco}`
+        produto += `nome: ${lista_de_produto[i].nome} produto: ${lista_de_produto[i].marca} preço ${lista_de_produto[i].preco}`
     }
     alert(produto)
 }
-function remover() {
-var remover = 
-lista_de_produto[i].splice(indice,1) = prompt("qualvocê deseja remover?")
 
-
-}
